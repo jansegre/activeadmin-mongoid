5ad0b0e3064ee94f5e9a3394d22448f40b0f3348
Gleb Tv <glebtv@gmail.com>
2012-11-15 14:29:01 +0400

    apply fix from here https://github.com/elia/activeadmin-mongoid/issues/23

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -2,7 +2,9 @@ module ActiveAdmin
   module Helpers
     module Collection
       def collection_size(collection=collection)
-        collection.count()
+        collection.count(true)
+      rescue
+        collection.count
       end
     end
   end
