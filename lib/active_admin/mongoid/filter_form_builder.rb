8730e388a8242796088fb28dc01c7c9514cc4b1a
Gleb Tv <glebtv@gmail.com>
2012-11-27 08:43:32 +0400

    Mongoid does not have integers for foreign keys

--- lib/active_admin/mongoid/filter_form_builder.rb
+++ lib/active_admin/mongoid/filter_form_builder.rb
@@ -1,14 +1,15 @@
 class ActiveAdmin::Filters::FormBuilder
   def default_input_type(method, options = {})
     if column = column_for(method)
-      p method, column
+      # p method, column.type.name.downcase
+      return :select if column.class.name == "Mongoid::Fields::ForeignKey"
+
       case column.type.name.downcase.to_sym
       when :date, :datetime, :time
         return :date_range
       when :string, :text, :object
         return :string
       when :integer
-        return :select if reflection_for(method.to_s.gsub('_id','').to_sym)
         return :numeric
       when :float, :decimal
         return :numeric
